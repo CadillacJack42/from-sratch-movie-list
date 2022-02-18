005271e7d9c72f84c73c243252feb13aea7c2031
James Keicher <jamesdkeicher@gmail.com>
2022-02-17 22:29:23 -0800

    Feat: fetch call to movie api with HARDCODE SEARCH, no render

--- src/Routes/WatchList.js
+++ src/Routes/WatchList.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function WatchList() {
+  const netlifyURL = `/.netlify/functions/list`;
+
+  useEffect(() => {
+    const response = searchMovies();
+    console.log(response);
+  }, []);
+
+  const searchMovies = async () => {
+    const response = await fetch(netlifyURL);
+    const json = await response.json();
+    console.log(json);
+  };
+
   return <div></div>;
 }
