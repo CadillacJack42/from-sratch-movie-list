36d8efa3d7b16254420996a832b3e60d87e24328
James Keicher <jamesdkeicher@gmail.com>
2022-02-17 17:17:01 -0800

    Chore: Navlinks set in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch, NavLink } from 'react-router-dom';
 import './App.css';
 import Auth from './Routes/Auth';
+import WatchList from './Routes/WatchList';
+import { logout } from './services/fetch-utils';
 
 function App() {
   const [currentUser, setUser] = useState(localStorage.getItem('supabase.auth.token'));
@@ -10,12 +12,22 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <h1>Welcome to Cadillac Jacks Movie Watchlist</h1>
+        <NavLink to={'/search'}>Search Page</NavLink> {' | '}
+        <NavLink to={'/watch-list'}>Watch List</NavLink> {' | '}
+        <NavLink onClick={() => logout()} to={'/'}>
+          Logout
+        </NavLink>
         <Route exact path={'/'}>
           {currentUser ? <Redirect to={'/watch-list'} /> : <Redirect to={'/auth'} />}
         </Route>
-        <Route exact path={'/auth'}>
-          <Auth setUser={setUser} />
-        </Route>
+        <Switch>
+          <Route exact path={'/auth'}>
+            <Auth setUser={setUser} />
+          </Route>
+          <Route exact path={'/watch-list'}>
+            <WatchList />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
---
+++ src/Routes/WatchList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { logout } from '../services/fetch-utils';
+
+export default function WatchList() {
+  return <div></div>;
+}
