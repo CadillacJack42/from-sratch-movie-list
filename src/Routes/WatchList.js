e6cf88440c01f3eed2765e87712ffd58e1195987
James Keicher <jamesdkeicher@gmail.com>
2022-02-24 09:56:26 -0800

    feat: notify user if movie in search is already on watch list

--- src/Routes/WatchList.js
+++ src/Routes/WatchList.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import WatchListMovie from '../Components/WatchListMovie';
+import { clearWatchList } from '../services/fetch-utils';
 
-export default function WatchList({ movieList, setMovieList }) {
+export default function WatchList({ movieList, setMovieList, currentUser, movieFetch }) {
+  const clearList = async () => {
+    await clearWatchList(currentUser.currentSession.user.id);
+    await movieFetch();
+  };
   return (
     <div>
+      <button onClick={() => clearList()}>Clear Watch List</button>
       {movieList.map((movie) => {
         return <WatchListMovie key={movie.id} movie={movie} setMovieList={setMovieList} />;
       })}
